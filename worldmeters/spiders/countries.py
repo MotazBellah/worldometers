e45c6c7942db1e2574db602bcb9221a1bdb7dc43
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-09-27 14:03:59 +0200

    Get the year and population from the relative links

--- worldmeters/spiders/countries.py
+++ worldmeters/spiders/countries.py
@@ -1,9 +1,10 @@
 import scrapy
+import logging
 
 
 class CountriesSpider(scrapy.Spider):
     name = 'countries'
-    allowed_domains = ['www.worldometers.info/']
+    allowed_domains = ['www.worldometers.info']
     start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
 
     def parse(self, response):
@@ -14,7 +15,22 @@ class CountriesSpider(scrapy.Spider):
             name = country.xpath(".//text()").get()
             link = country.xpath(".//@href").get()
 
+            # absoulte_url = f"https://www.worldometers.info{link}"
+            # absoulte_url = response.urljoin(link)
+
+            # yield scrapy.Request(url=absoulte_url)
+            # Follow the relative links
+            yield response.follow(url=link, callback=self.parse_country)
+
+    def parse_country(self, response):
+        # logging.info(response.url)
+        # Get the tabel that contain the year and population
+        rows = response.xpath("(//table[@class='table table-striped table-bordered table-hover table-condensed table-list'])[1]/tbody/tr")
+        for row in rows:
+            year = row.xpath(".//td[1]/text()").get()
+            population = row.xpath(".//td[2]/strong/text()").get()
+
             yield {
-                'country_name': name,
-                'country_link': link,
+                "year": year,
+                "population": population,
             }
